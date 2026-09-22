dbc85a53ad860d5aa4cec068f05969d957ebafad
hotabmax <maksim.solovev.99@inbox.ru>
2026-09-23 03:07:07 +0400

    changes v 1.1.0

--- main/views.py
+++ main/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Priority, Task
 
-# Create your views here.
+def task_list(request):
+    priorities = Priority.objects.all()
+    tasks = Task.objects.all()
+    return render(request, 'main/task/list.html', {'tasks': tasks, 'priorities': priorities})
